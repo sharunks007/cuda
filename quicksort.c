19360c5e562c5c08fe8da6d806fa630fad93b36b
Sharun Kumar <sharunks@ymail.com>
2016-10-11 10:27:17 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,18 +1,23 @@
+#include <cuda.h>
+
 __global__ void part(int *a, int *pos, int size) {
 	int *cp_a, i;
 	int pivot, left, right, t;
-	//el = threadIdx.x
 	cp_a = (int*)malloc(sizeof(int) * size);
-	//printf("\n Thread ID : %d",threadIdx.x);
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size; i++) { //create a copy of array a
 		cp_a[i] = a[i];
-		//printf("\n Copy array %d : %d",i,cp_a[i]);
 	}
+
+	//Swap first element with current (threadIdx.x element)
 	t = cp_a[threadIdx.x];
 	cp_a[threadIdx.x] = cp_a[0];
 	cp_a[0] = t;
+
+	//Set pivot
 	pivot = cp_a[0];
+
 	left = 1; right = size - 1;
+
 	while (left < right) {
 		while (left > 0 && left < size && cp_a[left] <= pivot)
 			left++;
@@ -26,6 +31,7 @@ __global__ void part(int *a, int *pos, int size) {
 	}
 	pos[threadIdx.x] = right;
 }
+
 int main() {
 	int *a, *dev_a, *pos, *dev_pos, i, *fin, sz;
 	printf("\n Enter the size of the array : ");
@@ -38,18 +44,12 @@ int main() {
 		scanf("%d", a + i);
 		fin[i] = INT_MAX;
 	}
-	/*      for(i = 0;i < sz;i++)
-	                printf("\n Element %d : %d",i,a[i]);
-	*/	cudaMalloc((void**)&dev_a, sizeof(int)*sz);
+	cudaMalloc((void**)&dev_a, sizeof(int)*sz);
 	cudaMalloc((void**)&dev_pos, sizeof(int)*sz);
-	//cudaMalloc((void**)&size,sizeof(int));
 	cudaMemcpy(dev_a, a, sz * sizeof(int), cudaMemcpyHostToDevice);
-	//cudaMemcpy(size,&sz,sizeof(int),cudaMemcpyHostToDevice);
 	part <<< 1, sz>>>(dev_a, dev_pos, sz);
 	cudaMemcpy(pos, dev_pos, sz * sizeof(int), cudaMemcpyDeviceToHost);
-	/*  for(i = 0;i < sz;i++)
-	    	printf("\n Position Array %d : %d",i,pos[i]);
-	*/  for (i = 0; i < sz; i++)
+	for (i = 0; i < sz; i++)
 		fin[pos[i]] = a[i];
 	for (i = sz - 1; i >= 0; i--)
 		if (fin[i] == INT_MAX)
